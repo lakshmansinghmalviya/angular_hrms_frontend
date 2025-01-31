b08776c552fe03fc6d8f04b532b5338c72c4068b
laskhman <dineshsolankhi3@gmail.com>
2025-02-01 02:42:18 +0530

    made the folder structure

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// echo "# angular_hrms_frontend" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/lakshmansinghmalviya/angular_hrms_frontend.git
-// git push -u origin main
-
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+server: {
+    hmr: {
+      timeout: 60000  
+    }
+  }
