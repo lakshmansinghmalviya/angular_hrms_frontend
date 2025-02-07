c2e30e87a1bf35b4591959ebbe73d457befc7f39
laskhman <dineshsolankhi3@gmail.com>
2025-02-07 23:24:00 +0530

    cutiya

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RegisterComponent } from './auth/register/register.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatCardModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
